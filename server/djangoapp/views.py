774882ff029fd4c4ccc5a51ebb54d678d63c2c4f
Mauricio A <acencio.mauricio.icinf@gmail.com>
2023-08-29 18:37:31 -0400

    add review test view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, analyze_review_sentiments
+from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, analyze_review_sentiments, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -99,6 +99,17 @@ def get_dealer_details(request, dealer_id):
         return HttpResponse('\n'.join(reviewstr))
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
+def add_review(request, dealer_id):
+    if request.user.is_authenticated and request.method == 'POST':
+        payload = {}
+        payload['id'] = 124
+        payload["review"] = review
+        payload["time"] = datetime.utcnow().isoformat()
+        payload["dealership"] = dealer_id
+        payload["name"] = 'Mauro Fern'
+        payload["purchase"] = False
 
+        url = 'https://us-south.functions.appdomain.cloud/api/v1/web/fcc6f7b6-c4ad-4067-a0a1-0287f313fa64/dealership-package/post-review'
+
+        result = post_request(url, payload)
+        return HttpResponse(result)        
