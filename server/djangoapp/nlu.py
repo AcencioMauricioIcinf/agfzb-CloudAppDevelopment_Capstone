1134fc70e04c2dcbede3f2144d9bf451443a31c0
Mauricio A <acencio.mauricio.icinf@gmail.com>
2023-09-06 17:04:16 -0400

    added env vars for api key

--- server/djangoapp/nlu.py
+++ server/djangoapp/nlu.py
@@ -1,9 +1,10 @@
-import json
+import json, os
 from ibm_watson import NaturalLanguageUnderstandingV1
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator, NoAuthAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
-authenticator = IAMAuthenticator('hpuzF87CSyiErL57hkWAmnmtc0-nvd62R8uxwIFQgT54')
+api_key = os.environ.get('NLU_API_KEY', '')
+authenticator = IAMAuthenticator(api_key) if api_key else NoAuthAuthenticator()
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
     authenticator=authenticator)
